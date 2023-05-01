7bfafcac03d3bfea99ccbd12d8db6b40ce36e2cb
Zippo00 <mikko.lempinen@student.oulu.fi>
2023-05-01 22:37:28 +0300

    5., 6., done, 7. done for one embedding method

--- thread_prediction.py
+++ thread_prediction.py
@@ -10,38 +10,153 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 
+
+def deg_feature_vectors(graphdata):
+    '''
+    Create degree distribution feature vectors for a given graph data set.
+    Feature vector is a fixed vector with the size of 10, where [Amount of nodes with degree of 1, 
+    Amount of nodes with degree of 2, Amount of nodes with degree of 3, Amount of nodes with degree of 4, 
+    Amount of nodes with degree of 5, Amount of nodes with degree of 6, Amount of nodes with degree of 7,
+    Amount of nodes with degree of 8, Amount of nodes with degree of 9, Amount of nodes with degree of 10+].
+    :param graphdata: (list) Data of each graph to generate a feature vector for.
+    :return: (list) Feature vectors.
+    '''
+    feature_vectors = []
+    for i in graphdata:
+        feature_vector = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        G = nx.Graph(i)
+        degree_sequence = sorted((d for n, d in G.degree()), reverse=True)
+        deg_values, deg_counts = np.unique(degree_sequence, return_counts=True)
+        for i, j in enumerate(deg_values):
+            if j == 1:
+                feature_vector[0] = deg_counts[i]
+            if j == 2:
+                feature_vector[1] = deg_counts[i]
+            if j == 3:
+                feature_vector[2] = deg_counts[i]
+            if j == 4:
+                feature_vector[3] = deg_counts[i]
+            if j == 5:
+                feature_vector[4] = deg_counts[i]
+            if j == 6:
+                feature_vector[5] = deg_counts[i]
+            if j == 7:
+                feature_vector[6] = deg_counts[i]
+            if j == 8:
+                feature_vector[7] = deg_counts[i]
+            if j == 9:
+                feature_vector[8] = deg_counts[i]
+            if j == 10:
+                feature_vector[9] = deg_counts[i]
+            if j == 11:
+                feature_vector[10] = deg_counts[i]
+            if j == 12:
+                feature_vector[11] = deg_counts[i]
+            if j == 13:
+                feature_vector[12] = deg_counts[i]
+            if j == 14:
+                feature_vector[13] = deg_counts[i]
+            if j > 14:
+                feature_vector[14] += deg_counts[i]
+        #print(f"Feature vector: {feature_vector}")
+        feature_vectors.append(feature_vector)
+    return feature_vectors
+
+def betweenness_centrality_vectors(graphdata):
+    '''
+    Create betweenness centrality feature vectors for a given graph data set.
+    '''
+    feature_vectors = []
+    for i in graphdata:
+        feature_vector = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        G = nx.Graph(i)
+        bc = nx.betweenness_centrality(G)
+        for key in bc:
+            if bc[key] == 0.0:
+                feature_vector[0] += 1
+            if 0.05 > bc[key] > 0.0:
+                feature_vector[1] += 1
+            if 0.1 > bc[key] >= 0.05:
+                feature_vector[2] += 1
+            if 0.2 > bc[key] >= 0.1:
+                feature_vector[3] += 1
+            if 0.3 > bc[key] >= 0.2:
+                feature_vector[4] += 1
+            if 0.4 > bc[key] >= 0.3:
+                feature_vector[5] += 1
+            if 0.5 > bc[key] >= 0.4:
+                feature_vector[6] += 1
+            if 0.6 > bc[key] >= 0.5:
+                feature_vector[7] += 1
+            if 0.7 > bc[key] >= 0.6:
+                feature_vector[8] += 1
+            if 0.8 > bc[key] >= 0.7:
+                feature_vector[9] += 1
+            if 0.9 > bc[key] >= 0.8:
+                feature_vector[10] += 1
+            if 1 > bc[key] >= 0.9:
+                feature_vector[11] += 1
+            if bc[key] == 1.0:
+                feature_vector[12] += 1
+        feature_vectors.append(feature_vector)
+    return feature_vectors
+
+def closeness_centrality_vectors(graphdata):
+    '''
+    Create closeness centrality feature vectors for a given graph data set.
+    '''
+    feature_vectors = []
+    for i in graphdata:
+        feature_vector = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        G = nx.Graph(i)
+        cc = nx.closeness_centrality(G)
+        for key in cc:
+            if cc[key] == 0.0:
+                feature_vector[0] += 1
+            if 0.05 > cc[key] > 0.0:
+                feature_vector[1] += 1
+            if 0.1 > cc[key] >= 0.05:
+                feature_vector[2] += 1
+            if 0.2 > cc[key] >= 0.1:
+                feature_vector[3] += 1
+            if 0.3 > cc[key] >= 0.2:
+                feature_vector[4] += 1
+            if 0.4 > cc[key] >= 0.3:
+                feature_vector[5] += 1
+            if 0.5 > cc[key] >= 0.4:
+                feature_vector[6] += 1
+            if 0.6 > cc[key] >= 0.5:
+                feature_vector[7] += 1
+            if 0.7 > cc[key] >= 0.6:
+                feature_vector[8] += 1
+            if 0.8 > cc[key] >= 0.7:
+                feature_vector[9] += 1
+            if 0.9 > cc[key] >= 0.8:
+                feature_vector[10] += 1
+            if 1 > cc[key] >= 0.9:
+                feature_vector[11] += 1
+            if cc[key] >= 1.0:
+                feature_vector[12] += 1
+        feature_vectors.append(feature_vector)
+    return feature_vectors
 # Initialize graph reader
 reader = GraphSetReader("reddit10k")
 
 # Reddit thread data
 graphs = reader.get_graphs()
 y = reader.get_target()
-
 # 10 Train-test random state values for deterministic set selections
 random_states = [0, 1, 5, 12, 38, 42, 47, 49, 72, 77]
-# List of feature vectors for each node (Degree Distribution as feature vector)
-# Feature vector = (Amount of nodes with degree of 1, Amount of nodes with degree of 2, Amount of nodes with degree of 3, Amount of nodes with degree of 4, Amount of nodes with degree of 5+)
-feature_vectors = []
-for i in graphs:
-    feature_vector = [0, 0, 0, 0, 0]
-    G = nx.Graph(i)
-    degree_sequence = sorted((d for n, d in G.degree()), reverse=True)
-    deg_values, deg_counts = np.unique(degree_sequence, return_counts=True)
-    for i, j in enumerate(deg_values):
-        if j == 1:
-            feature_vector[0] = deg_counts[i]
-        if j == 2:
-            feature_vector[1] = deg_counts[i]
-        if j == 3:
-            feature_vector[2] = deg_counts[i]
-        if j == 4:
-            feature_vector[3] = deg_counts[i]
-        if j > 4:
-            feature_vector[4] += deg_counts[i]
-    #print(f"Feature vector: {feature_vector}")
-    feature_vectors.append(feature_vector)
+
+# Generate feature vectors for each graph (Uncomment one of these to use)
+#feature_vectors = deg_feature_vectors(graphs)
+#feature_vectors = betweenness_centrality_vectors(graphs)
+feature_vectors = closeness_centrality_vectors(graphs)
+
 
     #                                                           **UNCOMMENT CODE BELOW TO PLOT THE GRAPH AND DEGREE DISTRIBUTION FOR EACH REDDIT THREAD**
     # fig = plt.figure("Degree of a graph", figsize=(8, 8))
@@ -65,23 +180,14 @@ for i in graphs:
     # plt.show()
 
 
-# USING EMBEDDING VECTOR FOR THE WHOLE NETWORK AS THE FEATURE VECTOR
-# Fit a Feather model to the graphs
-model = FeatherGraph()
-model.fit(graphs)
-# Get graph embedding
-X_embedding = model.get_embedding()
-
-
-
-
+print("\n--------------------------AUC Scores using a certain feature vector------------------------------------")
 # Predicting threads with Logistic Regression
 auc_mean = 0
 counter = 0
 for i in random_states:
     # 80/20 Train-test with degree distribution as predictors of the thread type
     X_train, X_test, y_train, y_test = train_test_split(feature_vectors, y, test_size=0.2, random_state=i)
-    # Use training data to predict the probability of a thread being discusison based with a logistic regression model
+    # Use training data to predict the probability of a thread being discussison based with a logistic regression model
     downstream_model = LogisticRegression(random_state=0, max_iter=1000).fit(X_train, y_train)
     y_hat = downstream_model.predict_proba(X_test)[:, 1]
     # Evaluate model performance
@@ -97,8 +203,8 @@ counter = 0
 for i in random_states:
     # 80/20 Train-test with degree distribution as predictors of the thread type
     X_train, X_test, y_train, y_test = train_test_split(feature_vectors, y, test_size=0.2, random_state=i)
-    # Use training data to predict the probability of a thread being discusison based with a logistic regression model
-    downstream_model = LinearDiscriminantAnalysis(random_state=0, max_iter=1000).fit(X_train, y_train)
+    # Use training data to predict the probability of a thread being discussison based with Linear Discriminant Analysis
+    downstream_model = LinearDiscriminantAnalysis().fit(X_train, y_train)
     y_hat = downstream_model.predict_proba(X_test)[:, 1]
     # Evaluate model performance
     auc = roc_auc_score(y_test, y_hat)
@@ -113,8 +219,8 @@ counter = 0
 for i in random_states:
     # 80/20 Train-test with degree distribution as predictors of the thread type
     X_train, X_test, y_train, y_test = train_test_split(feature_vectors, y, test_size=0.2, random_state=i)
-    # Use training data to predict the probability of a thread being discusison based with a logistic regression model
-    downstream_model = KNeighborsClassifier(random_state=0, max_iter=1000).fit(X_train, y_train)
+    # Use training data to predict the probability of a thread being discussison based with a K-Neighbors Classifier
+    downstream_model = KNeighborsClassifier().fit(X_train, y_train)
     y_hat = downstream_model.predict_proba(X_test)[:, 1]
     # Evaluate model performance
     auc = roc_auc_score(y_test, y_hat)
@@ -129,8 +235,8 @@ counter = 0
 for i in random_states:
     # 80/20 Train-test with degree distribution as predictors of the thread type
     X_train, X_test, y_train, y_test = train_test_split(feature_vectors, y, test_size=0.2, random_state=i)
-    # Use training data to predict the probability of a thread being discusison based with a logistic regression model
-    downstream_model = DecisionTreeClassifier(random_state=0, max_iter=1000).fit(X_train, y_train)
+    # Use training data to predict the probability of a thread being discussison based with a Decision Tree Classifier
+    downstream_model = DecisionTreeClassifier(random_state=0).fit(X_train, y_train)
     y_hat = downstream_model.predict_proba(X_test)[:, 1]
     # Evaluate model performance
     auc = roc_auc_score(y_test, y_hat)
@@ -145,8 +251,8 @@ counter = 0
 for i in random_states:
     # 80/20 Train-test with degree distribution as predictors of the thread type
     X_train, X_test, y_train, y_test = train_test_split(feature_vectors, y, test_size=0.2, random_state=i)
-    # Use training data to predict the probability of a thread being discusison based with a logistic regression model
-    downstream_model = GaussianNB(random_state=0, max_iter=1000).fit(X_train, y_train)
+    # Use training data to predict the probability of a thread being discussison based with Gaussian Naive Bayes
+    downstream_model = GaussianNB().fit(X_train, y_train)
     y_hat = downstream_model.predict_proba(X_test)[:, 1]
     # Evaluate model performance
     auc = roc_auc_score(y_test, y_hat)
@@ -161,8 +267,9 @@ counter = 0
 for i in random_states:
     # 80/20 Train-test with degree distribution as predictors of the thread type
     X_train, X_test, y_train, y_test = train_test_split(feature_vectors, y, test_size=0.2, random_state=i)
-    # Use training data to predict the probability of a thread being discusison based with a logistic regression model
-    downstream_model = SVC(random_state=0, max_iter=1000).fit(X_train, y_train)
+    # Use training data to predict the probability of a thread being discussison based with a support vector machine
+    downstream_model = make_pipeline(StandardScaler(), SVC(gamma='auto', probability=True))
+    downstream_model = downstream_model.fit(X_train, y_train)
     y_hat = downstream_model.predict_proba(X_test)[:, 1]
     # Evaluate model performance
     auc = roc_auc_score(y_test, y_hat)
@@ -170,3 +277,18 @@ for i in random_states:
     counter += 1
 auc_mean = auc_mean / counter
 print(f"Support Vector Machines Average AUC: {auc_mean:.4f}")
+
+
+# USING EMBEDDING VECTOR FOR THE WHOLE NETWORK AS THE FEATURE VECTOR
+print("------------------------------------------------AUC Scores using karate club embeddings with Logistic Regression--------------------------------------------------------------")
+# Fit a Feather model to the graphs
+model = FeatherGraph()
+model.fit(graphs)
+# Get graph embedding
+X_embeddings = model.get_embedding()
+
+X_train, X_test, y_train, y_test = train_test_split(X_embeddings, y, test_size=0.2, random_state=42)
+downstream_model = LogisticRegression(random_state=0).fit(X_train, y_train)
+y_hat = downstream_model.predict_proba(X_test)[:, 1]
+auc = roc_auc_score(y_test, y_hat)
+print('Embeddings Logistic Regression AUC: {:.4f}'.format(auc))
